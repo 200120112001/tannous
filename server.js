4e602e21227930c8e053f6148cc014a29bae54ae
200120112001 <khosseinatyyoussef@gmail.com>
2023-01-14 19:11:23 +0200

    added a new endpoint /data

--- server.js
+++ server.js
@@ -5,4 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+
+app.get('/data', function (req, res) {
+    var data =[{id:17,name:'omar'},
+    {id:54,name:'elia'},
+    {id:32,name:'fatat'},
+]
+res.send(data)
+  })
+
+
 app.listen(PORT)
